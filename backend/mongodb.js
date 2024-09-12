4d6f2f33d95bce3c9da6e0b57cc0157551e76396
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-09-12 19:39:09 +0530

    DB link changed

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -1,37 +1,40 @@
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
-const databaseName = 'happy';
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const { MongoClient } = require("mongodb");
+const url = "mongodb+srv://shwetasdhake16:shwetasdhake16@cluster0.ptpsd.mongodb.net/?retryWrites=true&w=majority&ssl=true&appName=Cluster0";
+const databaseName = "happy";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function getData() {
-    let db;
-    try {
-        // Connect to the database
-        await client.connect();
-        db = client.db(databaseName);
-        const collection = db.collection('task-manager');
+  let db;
+  try {
+    // Connect to the database
+    await client.connect();
+    db = client.db(databaseName);
+    const collection = db.collection("task-manager");
 
-        // Fetch and log existing data
-        let data = await collection.find({}).toArray();
-        console.log('Data before insertion:', data);
+    // Fetch and log existing data
+    let data = await collection.find({}).toArray();
+    console.log("Data before insertion:", data);
 
-        // Insert a new document
-        await collection.insertOne({
-            name: 'miko',
-            age: 21
-        });
+    // Insert a new document
+    await collection.insertOne({
+      name: "miko",
+      age: 21,
+    });
 
-        console.log('Document inserted successfully');
+    console.log("Document inserted successfully");
 
-        // Fetch and log data after insertion
-        data = await collection.find({}).toArray();
-        console.log('Data after insertion:', data);
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        // Close the connection in the finally block to ensure it's closed regardless of success or failure
-        await client.close();
-    }
+    // Fetch and log data after insertion
+    data = await collection.find({}).toArray();
+    console.log("Data after insertion:", data);
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    // Close the connection in the finally block to ensure it's closed regardless of success or failure
+    await client.close();
+  }
 }
 
 getData();
